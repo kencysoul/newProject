28ef1f34415789afbb68bd757f9374b03edcb285
kencysoul <johnkennethgalapia@gmail.com>
2024-03-20 10:38:39 +0800

    act 3 first commit

--- src/app/another/another.page.ts
+++ src/app/another/another.page.ts
@@ -1,22 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from '../authorization.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-another',
+  templateUrl: './another.page.html',
 })
-export class HomePage {
+export class AnotherPage implements OnInit {
 
   constructor(private authenticationService : AuthorizationService, private route : Router) {}
-  goToPage2() {
+  goToPage3() {
     if(this.authenticationService.authenticated == true){
-      this.route.navigate(['another'])
+      this.route.navigate(['third page'])
 
     }
     else{
-      this.route.navigate(['another/']);
+      this.route.navigate(['third page/']);
     }
     
   }
@@ -25,20 +24,25 @@ export class HomePage {
     console.log("Authenticated");
     alert("You are now Authenticated");
   }
+  
+  ngOnInit() {
+    this.authenticationService.authenticated = false;
+  }
 
   ionViewWillEnter() {
-    console.log("You will enter home page.")
+    console.log("You will enter second page.")
   }
 
   ionViewDidEnter() {
-    console.log("You did enter home page.")
+    console.log("You did enter second page.")
   }
 
   ionViewWillLeave() {
-    console.log("You will leave home page.")
+    console.log("You will leave second page.")
   }
 
   ionViewDidLeave() {
-    console.log("You did leave home page.")
+    console.log("You did leave second page.")
   }
 }
+//page 2
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AnotherPage } from './another/another.page';
+import { AuthorizationService } from './authorization.service';
+import { SharedPage } from './shared/shared.component';
+import { thirdPage } from './third page/third.page';
 
 const routes: Routes = [
   {
@@ -11,6 +15,24 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'my-custom-page',
+    loadChildren: () => import('./my-custom-page/my-custom-page.module').then( m => m.MyCustomPagePageModule)
+  },
+  {
+    path: 'another',
+    component: AnotherPage,
+    canActivate : [AuthorizationService]
+  },
+  {
+    path: 'third page',
+    component: thirdPage,
+    canActivate : [AuthorizationService]
+  },
+  {
+    path: 'shared',
+    component: SharedPage,
+  }
 ];
 
 @NgModule({
