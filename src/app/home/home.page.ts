fd0733c9de71e222737c89ad4cb66d130507c366
kencysoul <johnkennethgalapia@gmail.com>
2024-03-27 22:57:37 +0800

    6th commit: ui components

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,11 @@ export class HomePage implements OnInit{
   user: any;
   data: any;
 
-  constructor(private route : Router, private dataService : DataService) {}
+  constructor(private route : Router, private dataService : DataService,
+    private alertController : AlertController, private toastController : ToastController,
+    ) {}
+    volume = 100;
+
   ngOnInit(): void {
     this.user = localStorage.getItem('user');
     this.loadData();
@@ -28,5 +33,30 @@ export class HomePage implements OnInit{
   logout() {
     this.route.navigate(['login']);
     localStorage.removeItem('user');
+  }
+
+  async alert() {
+    const alert = await this.alertController.create({
+      header:'Alert!',
+      subHeader: 'alert message',
+      message: 'This is just an alert',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async toast() {
+    const toast = await this.toastController.create({
+      message: 'Toast component',
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  async dismissToast() {
+    const toast = await this.toastController.getTop();
+    if (toast){
+      toast.dismiss();
+    }
   }
 }
