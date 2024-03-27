27d911315775c63dfdec4a6e971117bb73c15378
kencysoul <johnkennethgalapia@gmail.com>
2024-03-27 18:38:04 +0800

    2nd commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +10,46 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {
   user: string = '';
   pw: string = '';
+  isToastOpen: boolean = false;
+  isValid: boolean = false;
   accounts: any[] =[
     {user: "admin", pw: "admin"}, {user: "user1", pw: "user1"}
   ]
-  constructor() { }
+  constructor(private route : Router, private alertControl : AlertController, private toastControl : ToastController) { }
 
   ngOnInit() {
   }
 
   async login() {
     
+    for (let i = 0; i < this.accounts.length; i++) {
+      if (this.accounts[i].user == this.user && this.accounts[i].pw == this.pw){
+        this.isValid = true;
+      }
+    }
+    if (this.isValid) {
+      const alert = await this.alertControl.create({
+        header: 'Login',
+        subHeader: 'Status',
+        message: 'Login Successful!',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+      setTimeout(() => {
+        this.route.navigate(['dashboard'])
+      }, 1000) //delay
+      
+    } else {
+      this.loginFailed()
+    }
+  }
+
+  async loginFailed(){
+    const toast = await this.toastControl.create({
+      message: 'Login Failed',
+      duration: 3000 //delay
+    });
+    toast.present();
   }
 }
