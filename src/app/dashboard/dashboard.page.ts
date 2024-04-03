d91a47a03ac9c28c369db570b69093c959c76238
kencysoul <johnkennethgalapia@gmail.com>
2024-04-03 20:17:45 +0800

    2nd commit

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService : AuthenticationService) { }
 
   ngOnInit() {
+    this.authenticationService.authenticate = false;
   }
 
 }
